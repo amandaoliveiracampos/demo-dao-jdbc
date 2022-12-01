09b615c9d4c508eaeebc61ef74c30f6da6d9d10d
Amanda Campos <amandaolivcampos@gmail.com>
2022-12-01 20:42:39 -0300

    DepartmentDao implementation and test

--- src/dao/model/DaoFactory.java
+++ src/dao/model/DaoFactory.java
@@ -1,12 +1,17 @@
 package dao.model;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
     public static SellerDao creatSellerDao() {
         return new SellerDaoJDBC(DB.getConnection());
+    }
+
+    public static DepartmentDao createDepartmentDao() {
+        return new DepartmentDaoJDBC(DB.getConnection());
     }
 
 }
